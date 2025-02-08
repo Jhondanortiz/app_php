e0658b9d708cda1be294a06217fa8b1f9b4a516d
Jhondanortiz <jhondanielortiz10@gmail.com>
2025-02-07 23:07:37 -0500

    Iniciando Proyecto

---
+++ models/User.php
@@ -0,0 +1,21 @@
+<?php
+
+    class User{
+        //Atributos
+        private $user_id;
+        private $user_lastname;
+        private $user_name;
+        private $user_birthday;
+        private $user_gender;
+        private $user_marital_status;
+        private $user_age;
+        private $user_address;
+        private $user_localphone;
+        private $user_cellphone;
+        private $user_email;
+        private $user_profession;
+
+
+    }
+
+?>
